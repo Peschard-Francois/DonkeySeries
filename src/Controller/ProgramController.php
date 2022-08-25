b9f209f543d78602cdf199af5887e09b6f630cfb
Peschard-Francois <francois.glenard@gmail.com>
2022-08-25 14:09:09 +0200

    Challenge3

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -6,13 +6,23 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/program', name: 'program_')]
 class ProgramController extends AbstractController
 {
-    #[Route('/program/', name: 'program_index')]
+    #[Route('/', name: 'program_index')]
     public function index(): Response
     {
         return $this->render('program/index.html.twig', [
             'website' => 'Donkey Séries',
+        ]);
+    }
+
+    #[Route('/{id}', name: 'program_show', requirements: ['id' => '\d+'], methods: ['GET'])]
+    public function show(string $id): Response
+    {
+        return $this->render('program/show.html.twig', [
+            'website' => 'Donkey Séries',
+            'programId' => $id
         ]);
     }
 }
